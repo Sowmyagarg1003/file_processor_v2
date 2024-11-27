8ba3600f841ae1902e23ee4701d52137b40b0b80
Sowmyagarg1003 <102238514+Sowmyagarg1003@users.noreply.github.com>
2024-11-27 12:50:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 DB_CONFIG = {
-    'dbname': 'file_processor',
-    'user': 'postgres',
-    'password': 'zxcvbnm',
-    'host': '192.168.1.12',
     'dbname': '',
     'user': '',
     'password': '',
